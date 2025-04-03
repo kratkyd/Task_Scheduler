b5a1ef7dfaf26ad5593066f133f3c273ca8fd957
SeasickSailor <152854736+SeasickSailor@users.noreply.github.com>
2025-04-04 01:39:22 +0200

    What I wanted to do in previous commit

--- unit_testing/unit_test.py
+++ unit_testing/unit_test.py
@@ -3,41 +3,42 @@ import click
 import project.script as script
 
 def test_validation():
-    cwd = os.getcwd()
+    import os
+    dir = os.path.dirname(os.path.abspath(__file__))
 
     #Expected inputs
-    task = script.Task("name", cwd+r"\project\script.py", "100", "P10H", True)
+    task = script.Task("name", dir+r"\project\script.py", "100", "P10H", True)
     assert task.validate()
 
-    task = script.Task("name", cwd+r"\unit_testing\file.exe", "1000", "P30D12H5M", True)
+    task = script.Task("name", dir+r"\unit_testing\file.exe", "1000", "P30D12H5M", True)
     assert task.validate()
 
-    task = script.Task("name", cwd+r"\project\script.py", "2", "P2M", True)
+    task = script.Task("name", dir+r"\project\script.py", "2", "P2M", True)
     assert task.validate()
 
     #Invalid path to program (expected false)
-    task = script.Task("name", cwd+r"\project\file.exe", "100", "P10H2M", True)
+    task = script.Task("name", dir+r"\project\file.exe", "100", "P10H2M", True)
     assert not task.validate()
 
     #Invalid interval (expected false)
-    task = script.Task("name", cwd+r"\project\script.py", "a", "P10H2M", True)
+    task = script.Task("name", dir+r"\project\script.py", "a", "P10H2M", True)
     assert not task.validate()
 
-    task = script.Task("name", cwd+r"\project\script.py", "-14", "P10H2M", True)
+    task = script.Task("name", dir+r"\project\script.py", "-14", "P10H2M", True)
     assert not task.validate()
 
-    task = script.Task("name", cwd+r"\project\script.py", "0", "P10H2M", True)
+    task = script.Task("name", dir+r"\project\script.py", "0", "P10H2M", True)
     assert not task.validate()
 
     #Invalid duration (expected false)
-    task = script.Task("name", cwd+r"\project\script.py", "100", "asdf", True)
+    task = script.Task("name", dir+r"\project\script.py", "100", "asdf", True)
     assert not task.validate()
 
-    task = script.Task("name", cwd+r"\project\script.py", "100", "4", True)
+    task = script.Task("name", dir+r"\project\script.py", "100", "4", True)
     assert not task.validate()
 
     #Interval greater than duration error (expected fail)
-    task = script.Task("name", cwd+r"\project\script.py", "200", "P2M", True)
+    task = script.Task("name", dir+r"\project\script.py", "200", "P2M", True)
     assert not task.validate()
 
 if __name__ == '__main__':
